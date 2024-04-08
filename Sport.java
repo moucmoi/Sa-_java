ccadde492eb6183cbd947d53d48e15d04baf069e
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-04-08 12:01:43 +0200

    créaion constructeur et getters

--- Sport.java
+++ Sport.java
@@ -1,3 +1,28 @@
-public class Sport{
-    
+import java.util.ArrayList;
+import java.util.List;
+public abstract class Sport{
+    protected String nomSport;
+    public List<Athlete> lesAthletes;
+    public List<Epreuve> lesEpreuves;
+
+
+public Sport(String nomSport) {
+    this.nomSport = nomSport;
+    this.lesAthletes = new ArrayList<>();
+    this.lesEpreuves = new ArrayList<>();
 }
+
+public String getNomSport() {
+    return nomSport;
+}
+
+public List<Athlete> getLesAthletes() {
+    return lesAthletes;
+}
+
+public List<Epreuve> getLesEpreuves() {
+    return lesEpreuves;
+}
+
+}
+
