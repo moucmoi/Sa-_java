fabf22a664ac7f3c60020c99f2a7a405d245369a
Amine-Yah <143809000+Amine-Yah@users.noreply.github.com>
2024-04-08 11:49:55 +0200

    Update Equipe.java

--- Equipe.java
+++ Equipe.java
@@ -1,3 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Equipe {
-    
+    private String nomEquipe;
+    private List<Athlete> athletes;
+
+    public Equipe(String nomEquipe){
+        this.nomEquipe = nomEquipe;
+        this.athletes = new ArrayList<>();
+    }
+
+    public void ajouteAthlete(Athlete athlete){
+        this.athletes.add(athlete);
+    }
+
+    public String getNom(){
+        return this.nomEquipe;
+    }
+
+    public List<Athlete> getLesAthletes(){
+        return this.athletes
+    }
+
+    @Override
+
+    public String toString(){
+        return "Equipe :" + this.nomEquipe + "/n" + "Athletes :" + this.athletes;
+    }
 }
