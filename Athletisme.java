ff8aedebdffc433c6c19c5f97691160fc8b134c1
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-04-15 12:12:05 +0200

    implémentation début Athletisme

--- Athletisme.java
+++ Athletisme.java
@@ -1,3 +1,11 @@
-public class Athletisme {
+public class Athletisme extends Sport{
     
+    public Athletisme(String nomSport) {
+        super(nomSport);
+    }
+    
+    @Override
+    public int calculerScore(Athlete athlete) {
+        return 0.5*athlete.getForce() + athlete.getAgilite() + 2*athlete.getEndurance();
+    }
 }
