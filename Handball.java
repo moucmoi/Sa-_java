e51523b3124b98a98f5b188912f0efcb1ef946a4
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-04-15 12:14:42 +0200

    Implémentation Handball

--- Handball.java
+++ Handball.java
@@ -1,3 +1,11 @@
-public class Handball {
-    
+public class Handball extends Sport {
+
+    public Handball(String nomSport) {
+        super(nomSport);
+    }
+
+    @Override
+    public int calculerScore(Athlete athlete) {
+        return athlete.getForce() + 1.5*athlete.getAgilite() + 1.5*athlete.getEndurance();
+    }
 }
