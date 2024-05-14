b885cc1877d1ebc3acc31c2fa9b2325c5af3ddb4
Noa Fonteny <noatoimoi@gmail.com>
2024-05-14 14:59:00 +0200

    début code éxecutable

--- Executable.java
+++ Executable.java
@@ -1,11 +1,17 @@
 import java.sql.*;
 
-public class Executable {
-    public static void main(String[] args) {
+public class Executable{
+    public static void main(String[] args) throws SQLException{
         Connection c;
-        Class.forName("com.mysql.jdbc.Dri");
-        c=DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/mabd","monlog","monpass");
-    
-
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/mabd","monlog","monpass");
+            //ICI CODE DU PROGRAMME
+        }
+        //gestion des exceptions
+        catch (SQLException ex){
+            System.out.println("Msg:"+ex.getMessage()+
+            ex.getErrorCode());
+        }
     }
 }
