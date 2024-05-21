19a5ee1162f71196f5b6c4f3257d9b063efaa3f8
Noa Fonteny <noatoimoi@gmail.com>
2024-05-21 15:16:30 +0200

    creation

--- Executable.java
+++ Executable.java
@@ -25,9 +25,29 @@ public class Executable{
         Epreuve RelaisLibreH=new Epreuve("Natation relais libre",true,false,Natation);
         Epreuve HandballH=new Epreuve("Handball", true, false,Handball);
         Epreuve  VolleyBallH=new Epreuve("Volley-Ball",true,false,VolleyBall);
-        Epreuve EscrimeFleuretH=new Epreuve("Escrime fleuret",true, false, Escrime);
-        Epreuve EscrimeEpeeH=new Epreuve("Escrime fleuret",true, false, Escrime);
+        Epreuve EscrimeFleuretH=new Epreuve("Escrime fleuret",true, true, Escrime);
+        Epreuve EscrimeEpeeH=new Epreuve("Escrime fleuret",true, true, Escrime);
+        Epreuve CentMetreHaiesH=new Epreuve("Athlétisme 110 haies", true, true, Athletisme);
+        Epreuve QuatreCentMetresRelaiH=new Epreuve("Athlétisme 110 haies", true, false, Athletisme);
         
+        Epreuve CentMetresF=new Epreuve("Natation 100 brasse", false, true, Natation);
+        Epreuve RelaisLibreF=new Epreuve("Natation relais libre",false,false,Natation);
+        Epreuve HandballF=new Epreuve("Handball", false, false,Handball);
+        Epreuve  VolleyBallF=new Epreuve("Volley-Ball",false,false,VolleyBall);
+        Epreuve EscrimeFleuretF=new Epreuve("Escrime fleuret",false, true, Escrime);
+        Epreuve EscrimeEpeeF=new Epreuve("Escrime fleuret",false, true, Escrime);
+        Epreuve CentMetreHaiesF=new Epreuve("Athlétisme 110 haies", false, true, Athletisme);
+        Epreuve QuatreCentMetresRelaiF=new Epreuve("Athlétisme 110 haies", false, false, Athletisme);
+
+        Equipe e1=new Equipe("Clebs");
+        Equipe e2=new Equipe("Furies")
+        
+        Athlete a1=new Athlete("Fonteny", "Noa", true, 100, 100, 100,France,e1);
+        Athlete a2=new Athlete("Chesneau", "Marin", false, 0, 0, 0, France, e1);
+        Athlete a3=new Athlete("Grandchamp", "Loris", true, 10, 10000, 50, France, e1);
+        Athlete a4=new Athlete("Yayaoui", "Mohamed-Amine", true, 10, 10, 10, Maroc, e2);
+
+
     }
    
    
