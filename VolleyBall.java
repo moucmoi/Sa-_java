388e431b1679b81111857b64102005405752ec96
Marin-univ <144822096+Marin-univ@users.noreply.github.com>
2024-05-21 15:22:33 +0200

    Bd (#25)
    * creation BD
    
    * suite de la base
    
    * debut insert
    
    * jsp
    
    * aaaa
    
    * modif
    
    * modification surEquipe
    
    ---------
    
    Co-authored-by: Marin Chesneau <marin.chsn@gmail.com>

--- VolleyBall.java
+++ VolleyBall.java
@@ -7,5 +7,5 @@ public class VolleyBall extends Sport{
     @Override
     public double calculerScore(Athlete athlete) {
         return 1.5*athlete.getForce() + athlete.getAgilite() + 1.5*athlete.getEndurance();
-    }
+    }   
 }
