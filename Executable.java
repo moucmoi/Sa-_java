4230c5a2f9fd0a5ed336a5b26306ca413c1f603a
moucmoi <143808893+moucmoi@users.noreply.github.com>
2024-05-21 14:22:42 +0200

    Executable (#21)
    * début
    
    * début code éxecutable
    
    * début executable

--- Executable.java
+++ Executable.java
@@ -1,12 +1,23 @@
 import java.sql.*;
 
-public class Executable {
-    public static void main(String[] args) {
-        Connection c;
-        Class.forName("com.mysql.jdbc.Dri");
-        c=DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/mabd","monlog","monpass");
-    
 
+public class Executable{
+    public static void main(String[] args) {
+        
     }
-}
+   
+   
+   
+    /* public static void main(String[] args) throws SQLException{
+        Connection c;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/mabd","monlog","monpass");
+        }
+        catch (SQLException ex){
+            System.out.println("Msg:"+ex.getMessage()+
+            ex.getErrorCode());
+        }
+    } */
+
  
