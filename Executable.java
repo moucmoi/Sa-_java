117668ee1a70861deb96b112386dcf9c87134980
Noa Fonteny <noatoimoi@gmail.com>
2024-05-21 15:16:04 +0200

    creation

--- Executable.java
+++ Executable.java
@@ -1,8 +1,32 @@
 import java.sql.*;
 
-
 public class Executable{
     public static void main(String[] args) {
+        
+        Pays France=new Pays("France","FRA");
+        Pays EtatsUnis=new Pays("USA","USA");
+        Pays Chine=new Pays("Chine","CHN");
+        Pays Japon=new Pays("Japon","JPN");
+        Pays Kenya=new Pays("Kenya","KEN");
+        Pays Maroc=new Pays("Maroc","MAR");
+        Pays Allemagne=new Pays("Allemagne","DEU");
+        Pays Australie=new Pays("Australie","AUS");
+        Pays Bresil=new Pays("Brésil","BRA");
+        Pays Turquie=new Pays("Turquie","TUR");
+
+
+        Natation Natation=new Natation();
+        Handball Handball=new Handball();
+        VolleyBall VolleyBall=new VolleyBall();
+        Escrime Escrime=new Escrime();
+        Athletisme Athletisme=new Athletisme();
+
+        Epreuve CentMetresH=new Epreuve("Natation 100 brasse", true, true, Natation);
+        Epreuve RelaisLibreH=new Epreuve("Natation relais libre",true,false,Natation);
+        Epreuve HandballH=new Epreuve("Handball", true, false,Handball);
+        Epreuve  VolleyBallH=new Epreuve("Volley-Ball",true,false,VolleyBall);
+        Epreuve EscrimeFleuretH=new Epreuve("Escrime fleuret",true, false, Escrime);
+        Epreuve EscrimeEpeeH=new Epreuve("Escrime fleuret",true, false, Escrime);
         
     }
    
@@ -19,5 +43,4 @@ public class Executable{
             ex.getErrorCode());
         }
     } */
-
- 
+}
