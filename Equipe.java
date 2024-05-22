cf49210c259c8471b740b5aaff2bce8d6a0dc9f2
moucmoi <noatoimoi@gmail.com>
2024-05-22 16:52:46 +0200

    changements equipe

--- Equipe.java
+++ Equipe.java
@@ -3,15 +3,36 @@ import java.util.ArrayList;
 
 public class Equipe {
     private String nomEquipe;
+    private Pays pays;
+    private List<Epreuve> lesEpreuves;
+
     private List<Athlete> lesAthletes;
 
-    public Equipe(String nomEquipe){
+    public Equipe(String nomEquipe,Pays pays){
         this.nomEquipe = nomEquipe;
+        this.pays=pays;
         this.lesAthletes = new ArrayList<>();
+        this.lesEpreuves=new ArrayList<>();
     }
 
     public void ajouteAthlete(Athlete athlete){
         this.lesAthletes.add(athlete);
+    }
+
+    public void ajouteEpreuve(Epreuve epreuve){
+        this.lesEpreuves.add(epreuve);
+    }
+
+    public Pays getPays() {
+        return pays;
+    }
+
+    public void setPays(Pays pays) {
+        this.pays = pays;
+    }
+
+    public List<Epreuve> getLesEpreuves() {
+        return lesEpreuves;
     }
 
     public String getNom(){
@@ -23,7 +44,6 @@ public class Equipe {
     }
 
     @Override
-
     public String toString(){
         return "Equipe :" + this.nomEquipe + "/n" + "Athletes :" + this.lesAthletes;
     }
