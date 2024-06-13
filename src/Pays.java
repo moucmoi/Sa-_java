8077e8529e2f6b5b06e0021d4613a2b1ebae391c
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-06-13 14:57:24 +0200

    javadoc de Pays

--- src/Pays.java
+++ src/Pays.java
@@ -10,6 +10,11 @@ public class Pays implements Comparable<Pays>{
     private List<Equipe> lesEquipes;
     
     public Pays(String nomPays, String codePays) {
+        /**
+         * Constructeur de la classe, instancie les nombres de médailles à 0 et la liste vide
+         * @param nomPays le nom du pays
+         * @param codePays le code du pays
+         */
         this.nomPays = nomPays;
         this.codePays = codePays;
         this.lesEquipes = new ArrayList<>();
@@ -19,37 +24,73 @@ public class Pays implements Comparable<Pays>{
     }
 
     public String getNomPays() {
+        /**
+         * Retourne le nom du pays 
+         * @return le nom du pays
+         */
         return nomPays;
     }
 
     public String getCodePays() {
+        /**
+         * Retourne le code du pays
+         * @return le code du pays
+         */
         return codePays;
     }
 
     public int getNbMedaille() {
+        /**
+         * Retourne le nombre de médaille total du pays
+         * @return nombre de médaille total du pays
+         */
         return this.nbMedailleOr + this.nbMedailleArgent + this.nbMedailleBronze;
     }
 
     public int getNbMedailleOr() {
+        /**
+         * Retourne le nombre de médaille en or du pays
+         * @return nombre de médaille en or du pays
+         */
         return nbMedailleOr;
     }
 
     public int getNbMedailleArgent() {
+        /**
+         * Retourne le nombre de médaille en argent du pays
+         * @return nombre de médaille en argent du pays
+         */
         return nbMedailleArgent;
     }
 
     public int getNbMedailleBronze() {
+        /**
+         * Retourne le nombre de médaille en bronze du pays
+         * @return nombre de médaille en bronze du pays
+         */
         return nbMedailleBronze;
     }
 
     public void ajouteEquipe(Equipe newEquipe) {
+        /**
+         * Ajoute une équipe au pays
+         * @param equipe l'Equipe à ajouter
+         */
         this.lesEquipes.add(newEquipe);
     }
 
     public List<Equipe> getLesEquipes() {
+        /**
+         * Retourne les équipes du pays
+         * @return les équipes du pays
+         */
         return this.lesEquipes;
     }
     public double calculerScore(){
+        /**
+         * Retourne le score pour les equipes du pays
+         * @return le score calculé
+         */
         double res=0;
         for(Equipe equipe:this.lesEquipes){
             res+=equipe.calculerScore();
@@ -58,24 +99,41 @@ public class Pays implements Comparable<Pays>{
     }
 
     public void ajouteOr(){
+        /**
+         * Ajoute une médaille d'or au pays
+         */
         this.nbMedailleOr+=1;
     }
 
     public void ajouteBronze(){
+        /**
+         * Ajoute une médaille de bronze au pays
+         */
         this.nbMedailleBronze+=1;
     }
 
     public void ajouteArgent(){
+        /**
+         * Ajoute une médaille d'argent au pays
+         */
         this.nbMedailleArgent+=1;
     }
 
     @Override
     public int compareTo(Pays p){
+        /**
+         * comparateur naturel du pays
+         * @param p un autre Pays
+         */
         return (int)this.calculerScore()-(int)p.calculerScore();
     }
 
     @Override
     public String toString() {
+        /**
+         * toString du pays
+         * @return le string
+         */
         return "Nom: " + this.nomPays +"\n"+ " Code: " + this.codePays + "\n" + " Nombre de Médailles" +"\n"+ " Or: " + this.nbMedailleOr +"\n"+ " Argent: " + this.nbMedailleArgent +"\n"+ " Bronze " + this.nbMedailleBronze;
     }
 }
