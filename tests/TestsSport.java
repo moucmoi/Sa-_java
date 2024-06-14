ed1a7799d18ab8bbad7a187d15ad5008d49a456f
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-06-14 16:07:18 +0200

    correction test Sport

--- tests/TestsSport.java
+++ tests/TestsSport.java
@@ -10,6 +10,8 @@ public class TestsSport {
     private static Sport athletisme;
     private static Athlete a1;
     private static Athlete a2; 
+    private static Pays france;
+    private static Equipe e1;
     
     @BeforeAll // initialisation faite en premier lieu
     static void init() { 
@@ -18,38 +20,42 @@ public class TestsSport {
         volleyBall=new Sport("VolleyBall",1.5,1,1.5);
         escrime=new Sport("Escrime",0.5,2.5,1);
         athletisme=new Sport("Athletisme",0.5,1,2.5);
-        a1 = new Athlete("Testeur", "Test", "H", 10, 10, 20);
-        a2 = new Athlete("Testeur2", "Test2", "F", 20, 10, 10);
+        france=new Pays("France","FRA");
+
+        e1=new Equipe("Equipe1",france);
+
+        a1 = new Athlete("Testeur", "Test", "H", 10, 10, 20, e1);
+        a2 = new Athlete("Testeur2", "Test2", "F", 20, 10, 10, e1);
     }
 
     @Test
     public void testNom() {
-        Assertions.assertEquals(natation.getNomSport, "Natation");
-        Assertions.assertEquals(handball.getNomSport, "Handball");
-        Assertions.assertEquals(volleyBall.getNomSport, "VolleyBall");
-        Assertions.assertEquals(escrime.getNomSport, "Escrime");
-        Assertions.assertEquals(athletisme.getNomSport, "Athletisme");
+        Assertions.assertEquals(natation.getNomSport(), "Natation");
+        Assertions.assertEquals(handball.getNomSport(), "Handball");
+        Assertions.assertEquals(volleyBall.getNomSport(), "VolleyBall");
+        Assertions.assertEquals(escrime.getNomSport(), "Escrime");
+        Assertions.assertEquals(athletisme.getNomSport(), "Athletisme");
     }
 
     @Test
     public void testCoeff() {
-        Assertions.assertEquals(natation.getCoeffForce, 1);
-        Assertions.assertEquals(handball.getCoeffForce, 1);
-        Assertions.assertEquals(volleyBall.getCoeffForce, 1.5);
-        Assertions.assertEquals(escrime.getCoeffForce, 0.5);
-        Assertions.assertEquals(athletisme.getCoeffForce, 0.5);
+        Assertions.assertEquals(natation.getCoeffForce(), 1);
+        Assertions.assertEquals(handball.getCoeffForce(), 1);
+        Assertions.assertEquals(volleyBall.getCoeffForce(), 1.5);
+        Assertions.assertEquals(escrime.getCoeffForce(), 0.5);
+        Assertions.assertEquals(athletisme.getCoeffForce(), 0.5);
 
-        Assertions.assertEquals(natation.getCoeffAgilite, 0.5);
-        Assertions.assertEquals(handball.getCoeffAgilite, 1.5);
-        Assertions.assertEquals(volleyBall.getCoeffAgilite, 1);
-        Assertions.assertEquals(escrime.getCoeffAgilite, 2.5);
-        Assertions.assertEquals(athletisme.getCoeffAgilite, 1);
+        Assertions.assertEquals(natation.getCoeffAgilite(), 0.5);
+        Assertions.assertEquals(handball.getCoeffAgilite(), 1.5);
+        Assertions.assertEquals(volleyBall.getCoeffAgilite(), 1);
+        Assertions.assertEquals(escrime.getCoeffAgilite(), 2.5);
+        Assertions.assertEquals(athletisme.getCoeffAgilite(), 1);
 
-        Assertions.assertEquals(natation.getCoeffEndurance, 2.5);
-        Assertions.assertEquals(handball.getCoeffEndurance, 1.5);
-        Assertions.assertEquals(volleyBall.getCoeffEndurance, 1.5);
-        Assertions.assertEquals(escrime.getCoeffEndurance, 1);
-        Assertions.assertEquals(athletisme.getCoeffEndurance, 2.5);
+        Assertions.assertEquals(natation.getCoeffEndurance(), 2.5);
+        Assertions.assertEquals(handball.getCoeffEndurance(), 1.5);
+        Assertions.assertEquals(volleyBall.getCoeffEndurance(), 1.5);
+        Assertions.assertEquals(escrime.getCoeffEndurance(), 1);
+        Assertions.assertEquals(athletisme.getCoeffEndurance(), 2.5);
     }
 
     @Test
