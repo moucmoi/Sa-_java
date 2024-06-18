f273339fe204659df4210cff30846b9a550adf6e
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-06-18 10:18:20 +0200

    ajout méthode listerPays

--- src/BD/OutilsRequete.java
+++ src/BD/OutilsRequete.java
@@ -1,5 +1,3 @@
-import java.sql.Statement;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
@@ -12,16 +10,30 @@ public class OutilsRequete {
         this.laConnexion = connexionMySQL;
     }
 
-    List<Integer> listerMedaillesPays(Pays pays) throws SQLException {
+    public List<Integer> listerMedaillesPays(Pays pays) throws SQLException {
         List<Integer> listeRetour = new ArrayList<>();
         this.st = this.laConnexion.createStatement();
         ResultSet rs = null;
-            String requete = "SELECT nb_medaille_or, nb_medaille_argent, nb_medaille_argent FROM PAYS WHERE code_pays =" + pays.getCodePays();
-            rs = st.executeQuery(requete);
+        String requete = "SELECT nb_medaille_or, nb_medaille_argent, nb_medaille_argent FROM PAYS WHERE code_pays =" + pays.getCodePays();
+        rs = st.executeQuery(requete);
         listeRetour.add(rs.getInt("nb_medaille_or"));
         listeRetour.add(rs.getInt("nb_medaille_argent"));
         listeRetour.add(rs.getInt("nb_medaille_bronze"));
+        rs.close();
 
+        return listeRetour;
+    }
+
+    public List<Pays> listerPays() throws SQLException {
+        List<Pays> listeRetour = new ArrayList<>();
+        this.st = this.laConnexion.createStatement();
+        ResultSet rs = null;
+            String requete = "SELECT * FROM PAYS";
+            rs = st.executeQuery(requete);
+            while (rs.next()){
+				listeRetour.add(new Pays(rs.getString("nom_pays"), rs.getString("code_pays"),rs.getInt("nb_medaille_or"),rs.getInt("nb_medaille_argent"),rs.getInt("nb_medaille_bronze")));
+			}
+			rs.close();
         return listeRetour;
     }
 }
