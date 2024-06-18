e85758fb268f54e667526df2eac85d395f09f455
Mohamed-Amine␣ Yahyaoui <mohamed-amine.yahyaoui@univ - orleans .fr>
2024-06-18 17:13:59 +0200

    Modifiaction de l'application

--- src/Vue/ControleurBoutonAccueil.java
+++ src/Vue/ControleurBoutonAccueil.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 
 public class ControleurBoutonAccueil implements EventHandler<ActionEvent> {
   
@@ -10,6 +11,13 @@ public class ControleurBoutonAccueil implements EventHandler<ActionEvent> {
     }
 
     public void handle(ActionEvent actionEvent) {
-        
+        Button boutonclique = (Button) actionEvent.getSource();
+        String textBouton = boutonclique.getText();
+        if (textBouton.contains("Connexion")) {
+            this.vueJO.pageConnexion();
+        }
+        if (textBouton.contains("Inscription")) {
+            this.vueJO.pageInscription();
+        }
     }
 }
--- src/Vue/ControleurDeconnexion.java
+++ src/Vue/ControleurDeconnexion.java
@@ -13,10 +13,10 @@ public class ControleurDeconnexion implements EventHandler<ActionEvent> {
     }
 
     public void handle(ActionEvent actionEvent) {
-        Optional<ButtonType> reponse = this.vueJO.popUpDeconnexion().showAndWait();
-        if (reponse.isPresent() && reponse.get().equals(ButtonType.YES)){
+    //     Optional<ButtonType> reponse = this.vueJO.popUpDeconnexion().showAndWait();
+    //     if (reponse.isPresent() && reponse.get().equals(ButtonType.YES)){
             
-            this.vueJO.PageAccueil();
-        }
+    //         this.vueJO.PageAccueil();
+    //     }
     }
 }
